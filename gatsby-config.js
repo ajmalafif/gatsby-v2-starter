e57d4d7ef394d673948eddbb3b89b8653acc374b
Ajmal Afif Jamal <ajmal.jamal@ITSG002907-MAC.local>
2018-08-14 10:47:50 +0800

    Add helmet and offline worker

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,22 @@
-// In your gatsby-config.js
-plugins: [
-  {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      name: 'Ajmal Afif',
-      short_name: 'Ajmal Afif',
-      start_url: '/',
-      background_color: '#fff',
-      theme_color: '#05f',
-      display: 'minimal-ui',
-      icon: 'src/images/icon.png', // This path is relative to the root of the site.
-    },
+module.exports = {
+  siteMetadata: {
+    siteUrl: 'https://gatsby-v2-starter.netlify.com',
+    title: 'Ajmal Afif\'s personal website 👨🏻‍💻'
   },
-  'gatsby-plugin-offline'
-]
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'Ajmal Afif',
+        short_name: 'Ajmal Afif',
+        start_url: '/',
+        background_color: '#fff',
+        theme_color: '#05f',
+        display: 'minimal-ui',
+        icon: 'src/images/icon.png', // This path is relative to the root of the site.
+      },
+    },
+    'gatsby-plugin-offline'
+  ]
+}
